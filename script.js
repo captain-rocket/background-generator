ab878dbee84da78f80e4e82b98f9dd07606229f7
rogerb45365 <59418774+rogerb45365@users.noreply.github.com>
2020-03-28 13:59:04 -0400

    adding starting project

---
+++ script.js
@@ -0,0 +1,38 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.getElementById("button");
+setGradient();
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function randomColor(button) {
+	color1.value = getRandomColor1();
+	color2.value = getRandomColor1();
+	setGradient();
+}
+
+function getRandomColor1() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+button.addEventListener("click", randomColor);
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
